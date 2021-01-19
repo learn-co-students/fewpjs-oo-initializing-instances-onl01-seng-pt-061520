b2d8b78eadcfad3986878a7334b34dae873c5bf4
rhday <54642787+rhday@users.noreply.github.com>
2021-01-19 16:28:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {//use 'constructor' and pass in desired args
+        this.food = food
+        this.drink = drink
+    }
+}
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+class Dinner {
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._dessert = dessert//this means thatt 'undefined' will equal whatever has been passed in to dessert.
+    }
+}
